aea24a02f043258978c1f216f4eda02f9d5b8977
Shreyas Miraj <shreyasmiraj8@gmail.com>
2017-09-13 20:35:11 +0530

    special matrix 0.2 - fixed comments and variable initialization

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,12 @@
 ## functions do
 
 ## Write a short comment describing this function
+## makeCacheMatrix returns a list with the orginal matrix and its inverse
+## set method within is used to initialize the cached data
+## get method within is used to retrive the cached data
 
 makeCacheMatrix <- function(x = matrix()) {
-  i <- matrix()
+  i <- NULL
   set<-function(y){
       x <<- y
       i <<- NULL
@@ -19,6 +22,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## cacheSolve utilizises the list created by makeCache matrix
+## if the inverse set there is inverse then new inverse will 
+## be calculated by using Solve method over the matrix
+## the new inverse will be stored within the list again using the setinverse
+## if its not null then the value will be directly returned
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -27,8 +35,8 @@ cacheSolve <- function(x, ...) {
     if(!is.null(m)){
         message("getting cached data");
         return(m)
-      }
-      data <- x$get()
-      m <- Solve(data)
-      x$setinverse(m)
+    }
+    data <- x$get()
+    m <- Solve(data)
+    x$setinverse(m)
 }
